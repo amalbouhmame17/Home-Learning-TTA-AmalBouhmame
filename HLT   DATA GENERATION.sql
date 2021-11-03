ce0d3f7ea30f487e24dab013ff12b426ffb656be
Amal Bouhmame <78333660+amalbouhmame17@users.noreply.github.com>
2021-11-03 12:28:12 +0000

    Add files via upload

---
+++ HLT   DATA GENERATION.sql
@@ -0,0 +1,10 @@
+--- task 1
+
+SELECT SUBSTRING('Please find the substring in this string',17,9);
+
+--- task 2
+
+SELECT ABS(-25.76823), SIGN(-25.76823), ROUND(-25.76823, 2);
+
+--- task 3
+SELECT EXTRACT(MONTH FROM CURRENT_DATE());
